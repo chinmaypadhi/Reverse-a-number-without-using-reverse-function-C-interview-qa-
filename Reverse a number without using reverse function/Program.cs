96c4067bd42c950f5c27c2d527085d3918d49686
chinmay padhi <padhichinmay108@gmail.com>
2024-12-08 12:28:16 +0530

    first commit

---
+++ Reverse a number without using reverse function/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reverse_a_number_without_using_reverse_function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
